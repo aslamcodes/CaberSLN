d287b77d8380b208c04a43bb0769b67635fa5246
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-21 22:06:01 +0530

    Add Driver Model

---
+++ Caber/Models/Driver.cs
@@ -0,0 +1,15 @@
+﻿namespace Caber.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+
+        public required string LicenseNumber { get; set; }
+
+        public DateOnly LicenseExpiryDate { get; set; }
+
+        //public ICollection<Cab> OwnedCabs { get; set; }
+
+        //public ICollection<DriverRating> DriverRatings { get; set; }
+    }
+}
