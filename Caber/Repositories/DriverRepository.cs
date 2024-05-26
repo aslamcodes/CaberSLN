b487361457f22bafa90a783e7c57409faf7b75fd
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-26 16:25:55 +0530

    Add DriverRepository And Tests

---
+++ Caber/Repositories/DriverRepository.cs
@@ -0,0 +1,92 @@
+﻿using Caber.Contexts;
+using Caber.Exceptions;
+using Caber.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caber.Repositories
+{
+    public class DriverRepository(CaberContext context) : IRepository<int, Driver>
+    {
+        public async Task<Driver> Add(Driver entity)
+        {
+            try
+            {
+                context.Drivers.Add(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<Driver> Delete(int key)
+        {
+            try
+            {
+                var driver = await GetByKey(key);
+                context.Drivers.Remove(driver);
+                await context.SaveChangesAsync();
+                return driver;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Driver>> GetAll()
+        {
+            try
+            {
+                var drivers = await context.Drivers.ToListAsync();
+
+                return drivers;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<Driver> GetByKey(int key)
+        {
+            try
+            {
+                var driver = await context.Drivers.FindAsync(key);
+
+                return driver ?? throw new DriverNotFoundException(key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<Driver> Update(Driver entity)
+        {
+            try
+            {
+                context.Drivers.Update(entity);
+
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ CaberTests/RepositoryTests/DriverRepositoryTests.cs
@@ -0,0 +1,191 @@
+﻿using Caber.Contexts;
+using Caber.Exceptions;
+using Caber.Models;
+using Caber.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaberTests.RepositoryTests
+{
+    public class DriverRepositoryTests
+    {
+        private CaberContext context;
+
+        private CaberContext GetContext()
+        {
+            return context;
+        }
+
+        private void SetContext(CaberContext value)
+        {
+            context = value;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<CaberContext>()
+                .UseInMemoryDatabase("EmployeeTest")
+            .Options;
+
+            SetContext(new CaberContext(options));
+            GetContext().Database.EnsureCreated();
+
+            var user = new User()
+            {
+                Email = "123@gmail.com",
+                FirstName = "John",
+                Password = new byte[] { 1, 2, 3, 4 },
+                PasswordHashKey = new byte[] { 1, 2, 3, 4 },
+                Phone = "123123",
+                Address = "123"
+            };
+
+            GetContext().Users.Add(user);
+            GetContext().SaveChanges();
+        }
+
+        [Test]
+        public async Task AddDriver()
+        {
+            #region Arrange
+            IRepository<int, Driver> repository = new DriverRepository(GetContext());
+
+            Driver driver = new()
+            {
+                UserId = 1,
+                OwnedCabs = null,
+                LicenseExpiryDate = DateOnly.FromDayNumber(10),
+                LicenseNumber = "123456"
+            };
+            #endregion
+
+            #region Action
+            _ = await repository.Add(driver);
+
+            Driver driver1 = await repository.GetByKey(1);
+            #endregion
+
+            #region Assert
+            Assert.That(driver1, Is.EqualTo(driver));
+            Assert.Multiple(() =>
+            {
+                Assert.That(driver1.UserId, Is.EqualTo(1));
+                Assert.That(driver1.LicenseNumber, Is.EqualTo("123456"));
+            });
+            #endregion
+        }
+
+        [Test]
+        public async Task DeleteDriver()
+        {
+            #region Arrange
+            IRepository<int, Driver> repository = new DriverRepository(GetContext());
+
+            Driver driver = new()
+            {
+                UserId = 1,
+                OwnedCabs = null,
+                LicenseExpiryDate = DateOnly.FromDayNumber(10),
+                LicenseNumber = "123456"
+            };
+
+            _ = await repository.Add(driver);
+
+            #endregion
+
+            #region Action
+            _ = await repository.Delete(1);
+            #endregion
+
+            #region Assert
+            Assert.ThrowsAsync<DriverNotFoundException>(async () => await repository.GetByKey(1));
+            #endregion 
+        }
+
+        [Test]
+        public async Task GetAllDrivers()
+        {
+            #region Arrange
+            IRepository<int, Driver> repository = new DriverRepository(GetContext());
+
+            Driver driver1 = new Driver()
+            {
+                UserId = 1,
+                OwnedCabs = null,
+                LicenseExpiryDate = DateOnly.FromDayNumber(10),
+                LicenseNumber = "123456"
+            };
+
+            Driver driver2 = new Driver()
+            {
+                UserId = 2,
+                OwnedCabs = null,
+                LicenseExpiryDate = DateOnly.FromDayNumber(15),
+                LicenseNumber = "789012"
+            };
+
+            Driver driver3 = new Driver()
+            {
+                UserId = 3,
+                OwnedCabs = null,
+                LicenseExpiryDate = DateOnly.FromDayNumber(20),
+                LicenseNumber = "345678"
+            };
+
+            _ = await repository.Add(driver1);
+            _ = await repository.Add(driver2);
+            _ = await repository.Add(driver3);
+
+            #endregion
+
+            #region Action
+            var drivers = await repository.GetAll();
+            #endregion
+
+            #region Assert
+            Assert.That(drivers.Count(), Is.EqualTo(3));
+            #endregion
+        }
+
+        [Test]
+        public async Task UpdateDriverTest()
+        {
+            #region Arrange
+            IRepository<int, Driver> repository = new DriverRepository(GetContext());
+
+            Driver driver = new()
+            {
+                UserId = 1,
+                OwnedCabs = null,
+                LicenseExpiryDate = DateOnly.FromDayNumber(10),
+                LicenseNumber = "123456"
+            };
+            _ = await repository.Add(driver);
+
+            #endregion
+
+            #region Action
+            var driverDb = await repository.GetByKey(1);
+            driverDb.LicenseNumber = "654321";
+            driverDb.LicenseExpiryDate = DateOnly.FromDateTime(DateTime.Now.AddDays(10));
+            await repository.Update(driverDb);
+
+            #endregion
+
+            #region Assert
+            var updatedDriver = await repository.GetByKey(1);
+            Assert.Multiple(() =>
+            {
+                Assert.That(updatedDriver.LicenseNumber, Is.EqualTo("654321"));
+                Assert.That(updatedDriver.LicenseExpiryDate, Is.EqualTo(DateOnly.FromDateTime(DateTime.Now.AddDays(10))));
+            });
+            #endregion
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GetContext().Database.EnsureDeleted();
+        }
+    }
+}
