2dabd19ae325e9d09942b316c0a51effa8aa84ac
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-27 23:42:58 +0530

    feat(logging): integrate log4net for logging support

--- Caber/Program.cs
+++ Caber/Program.cs
@@ -25,6 +25,8 @@ namespace Caber
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
 
+            builder.Services.AddLogging(l => l.AddLog4Net());
+
             #region SwaggerGen
 
             builder.Services.AddSwaggerGen(c =>
